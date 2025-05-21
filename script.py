b1b99ab9e9af7d4496b06dbf1c3fb7cccf199a88
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-21 16:19:18 +0200

    feat: add function for finding ther Dockerfile inside a directory

--- script.py
+++ script.py
@@ -2,8 +2,20 @@ import docker
 import os
 import sys
 
+DOCKER_TAG_FORMAT: str = "europe-west8-docker.pkg.dev/er-climate-monitor/quickstart-docker-repo/%s"
+
+
+def find_dockerfile(path: str) -> str | None:
+    elements: list[str] = os.listdir(path)
+    for element in elements:
+        if element == "Dockerfile":
+            return os.path.join(path, element)
+    return None
+
 
 def build_image(service_folder: str) -> None:
+    dockerfile_path: str = find_dockerfile(service_folder)
+    return
 
 
 def main():
