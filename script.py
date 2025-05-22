3ecbf81859e08cfa3b906ce2bcbf0277a7ee982f
matteo.iorio2 <matteo.iorio01@gmail.com>
2025-05-22 09:37:46 +0200

    feat: add new function for automatically deploy a container on Cloud Run

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 import os
 import sys
 from python_on_whales import docker, Image
-
+import subprocess
 
 DOCKER_TAG_FORMAT: str = "europe-west8-docker.pkg.dev/er-climate-monitor/quickstart-docker-repo/{}"
+GCP_URL = "https://run.googleapis.com/v2/projects/e-climate-monitor/locations/europe-west8/services?serviceId={}"
 
 
 def find_dockerfile(path: str) -> str | None:
@@ -17,9 +18,19 @@ def find_dockerfile(path: str) -> str | None:
 def build_image(service_folder: str, service_name: str) -> None:
     dockerfile_path: str = find_dockerfile(service_folder)
     if dockerfile_path:
-        builded_image: Image = docker.build(
-            context_path=service_folder, file=dockerfile_path, tags=[DOCKER_TAG_FORMAT.format(service_name)], push=True, platforms=["linux/amd64"])
+        formatted_tag: str = DOCKER_TAG_FORMAT.format(service_name)
+        docker.build(
+            context_path=service_folder, file=dockerfile_path, tags=[formatted_tag], push=True, platforms=["linux/amd64"])
+        deploy_service(formatted_tag, service_name)
+
     return
+
+
+def deploy_service(docker_tag: str, service_name: str) -> None:
+    # deploy
+    deploy_command: str = "gcloud run deploy {} --image {} --project er-climate-monitor --region europe-west8 --allow-unauthenticated".format(
+        service_name, docker_tag)
+    subprocess.run(deploy_command.split(' '))
 
 
 def main():
